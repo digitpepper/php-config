ba7dd0ae9856ad99b8b287a9836330e604fa1c8d
digitpepper <postmaster@digitpepper.com>
2024-10-09 17:42:19 +0800

    first commit

---
+++ src/Config.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace DP;
+
+class Config
+{
+	/**
+	 * @var string
+	 */
+	protected static $dir = '';
+
+	/**
+	 * @var array<string, array<string, mixed>>
+	 */
+	protected static $data = [];
+
+	/**
+	 * @return string
+	 */
+	public static function get_dir()
+	{
+		return self::$dir;
+	}
+
+	/**
+	 * @param string $dir
+	 */
+	public static function set_dir(string $dir)
+	{
+		self::$dir = realpath($dir);
+	}
+
+	/**
+	 * @param string $config
+	 * @param string|null $key
+	 * @return mixed
+	 */
+	public static function get(string $config, string $key = null)
+	{
+		$path = self::$dir . "/$config.php";
+		if (!isset(self::$data[$path])) {
+			self::$data[$path] = require $path;
+		}
+		return $key === null ? self::$data[$path] : self::$data[$path][$key];
+	}
+}
